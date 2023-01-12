25d6296a4ad12b70f662041709f0adca8e27e23f
Michelon Souza <michelon.souza@hotmail.com>
2023-01-12 20:18:27 -0300

    feat: add api-url type to .env file

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -4,9 +4,10 @@ type EnvType = 'development' | 'staging' | 'production' | 'test';
 type BooleanType = 'true' | 'false';
 
 interface ImportMetaEnv {
+  readonly VITE_APP_API_URL: string;
+  readonly VITE_APP_NODE_ENV: EnvType;
   readonly VITE_APP_ENCRYPT_STORAGE_PREFIX: string;
   readonly VITE_APP_ENCRYPT_STORAGE_SECRET_KEY: string;
-  readonly VITE_APP_NODE_ENV: EnvType;
 }
 
 interface ImportMeta {
