5ac4bfd124c77583a8845fc328a135922b69aefb
Michelon Souza <michelon.souza@hotmail.com>
2023-01-13 13:54:53 -0300

    feat: initial configure tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  darkMode: ['class', '[data-mode="dark"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
