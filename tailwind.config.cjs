09723f0297bbe03c0b421caf722b604914f7ad74
Michelon Souza <michelon.souza@hotmail.com>
2023-01-23 19:10:00 -0300

    feat: implements base-theme to tailwind.css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,19 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   darkMode: ['class', '[data-mode="dark"]'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF007C',
+        secondary: '#54595F',
+        terciary: '#2F1D57',
+        text: '#7A7A7A',
+        accent: '#61CE70',
+        'accent-dark': '#10C300',
+      },
+      fontFamily: {
+        sans: ['Montserrat Alternates', 'Arial', 'Helvetica', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
