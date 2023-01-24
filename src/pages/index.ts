42c791ae3946eb56511a651e2c5f591f40cafcb1
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 20:25:33 -0300

    feat: esport CartPage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as CartPage } from './CartPage.vue';
+export { default as CartPage } from './CartPage/index.vue';
 export { default as CategoryProductPage } from './CategoryProductPage.vue';
 export { default as HomePage } from './HomePage.vue';
 export { default as ProductPage } from './ProductPage.vue';
