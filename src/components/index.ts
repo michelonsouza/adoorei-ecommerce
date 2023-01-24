043df0cb56bc603ac774166acf8b8cd232e6f73b
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 12:19:50 -0300

    feat: export SelectInput component and your types

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { default as HeroBanner } from './HeroBanner.vue';
 export { default as ProductCard } from './ProductCard.vue';
 export { default as ProductList } from './ProductList.vue';
 export { default as SearchInput } from './SearchInput.vue';
+export { default as SelectInput } from './SelectInput.vue';
+export * from './SelectInput.vue';
