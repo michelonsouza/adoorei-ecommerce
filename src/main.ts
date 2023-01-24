a8d8e5d7f9808d50e829fafc8bc0060b93f6aaa9
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 10:01:11 -0300

    feat: use pinia and router into app

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue';
 
 import './index.css';
-import './style.css';
 import App from './App.vue';
 import { router } from './router';
+import { pinia } from './store';
 
 export const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
 
 app.mount('#app');
