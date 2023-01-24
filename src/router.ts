07b5e192d9de5c10e76f2ee992b3d82e2b3a378e
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 20:21:58 -0300

    feat: add cart  page

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
 
 import { HomePage } from './pages';
 
-const CartPage = () => import('./pages/CartPage.vue');
+const CartPage = () => import('./pages/CartPage/index.vue');
 const CategoryProductPage = () => import('./pages/CategoryProductPage.vue');
 const ProductPage = () => import('./pages/ProductPage.vue');
 
