8c764e5fbdac1be4e14a582a7e310dcc78817fe3
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 10:13:10 -0300

    feat: add HeroBanner component to use in HomePage

---
+++ src/components/HeroBanner.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="w-full relative rounded-lg h-64 p-4 flex align-top bg-cover bg-no-repeat bg-center after:w-full after:h-full after:absolute after:z-10 after:top-0 after:left-0"
+    :style="{ 'background-image': `url(${bannerImg})` }"
+  >
+    <h1 class="text-4xl text-white">adoorei</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import bannerImg from '@/assets/images/clark-street-mercantile-unsplash.jpeg';
+</script>
+
+<script lang="ts">
+export default {
+  name: 'HeroBanner',
+};
+</script>
