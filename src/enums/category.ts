5b6dc99be8c0ca7b73ca4357c15623588ea73177
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 10:15:25 -0300

    feat: add new category enums

--- src/enums/category.ts
+++ src/enums/category.ts
@@ -5,4 +5,19 @@ export enum CategoryEnum {
   WOMENS_CLOATING = "women's clothing",
 }
 
+export enum CategoryTagEnum {
+  electronics = CategoryEnum.ELETRONICS,
+  jewelery = CategoryEnum.JEWELERY,
+  'men-s-cloating' = CategoryEnum.MENS_CLOATING,
+  'women-s-cloating' = CategoryEnum.WOMENS_CLOATING,
+}
+
+export enum CategoryToTagEnum {
+  'electronics' = 'electronics',
+  'jewelery' = 'jewelery',
+  "men's clothing" = 'men-s-cloating',
+  "women's clothing" = 'women-s-cloating',
+}
+
 export type CategoryKeys = keyof typeof CategoryEnum;
+export type CategoryTagKeys = keyof typeof CategoryTagEnum;
