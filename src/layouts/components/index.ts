8362278620f6bbe440812178db8701a2325cd97e
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 10:18:17 -0300

    feat: export all components to layouts

---
+++ src/layouts/components/index.ts
@@ -0,0 +1,4 @@
+export { default as AppFooter } from './AppFooter.vue';
+export { default as AppHeader } from './AppHeader.vue';
+export { default as AppSidebar } from './AppSidebar.vue';
+export { default as ThemeModeSwitch } from './ThemeModeSwitch.vue';
