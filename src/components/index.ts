2cc4f60ef8aeeecbc0d4e5ddee9f48e8eeec5ae7
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 20:22:26 -0300

    feat: export all components from components folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export { default as HeroBanner } from './HeroBanner.vue';
+export { default as LoadingOverlay } from './LoadingOverlay.vue';
 export { default as ProductCard } from './ProductCard.vue';
 export { default as ProductList } from './ProductList.vue';
 export { default as SearchInput } from './SearchInput.vue';
 export { default as SelectInput } from './SelectInput.vue';
+export { default as StyledInput } from './StyledInput.vue';
 export * from './SelectInput.vue';
