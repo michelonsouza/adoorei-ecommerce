7a98e0d5e1cb013d079c6d366f1af60329feddd7
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 20:23:35 -0300

    fix: remove old component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,4 @@ export { default as ProductCard } from './ProductCard.vue';
 export { default as ProductList } from './ProductList.vue';
 export { default as SearchInput } from './SearchInput.vue';
 export { default as SelectInput } from './SelectInput.vue';
-export { default as StyledInput } from './StyledInput.vue';
 export * from './SelectInput.vue';
