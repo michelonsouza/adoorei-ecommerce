0e374c4ce490144335f445ad6e4cfb11412ee34e
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 20:23:00 -0300

    feat: create SelectInput component

--- src/components/SelectInput.vue
+++ src/components/SelectInput.vue
@@ -3,14 +3,14 @@
     <Listbox v-model="selectedValue" @update:model-value="handleChange">
       <div class="relative">
         <ListboxButton
-          class="relative w-full cursor-default rounded-lg bg-white dark:bg-gray-500 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm z-50"
+          class="relative w-full cursor-default rounded-lg bg-white dark:bg-gray-500 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-primary sm:text-sm z-10"
         >
           <span class="block truncate">{{ selectedValue.label }}</span>
           <span
             class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
           >
             <ChevronUpDownIcon
-              class="h-5 w-5 text-gray-400"
+              class="h-5 w-5 text-gray-400 dark:text-white"
               aria-hidden="true"
             />
           </span>
@@ -22,7 +22,7 @@
           leave-to-class="opacity-0"
         >
           <ListboxOptions
-            class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-30"
+            class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white dark:bg-gray-300 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-10"
           >
             <ListboxOption
               v-for="option in props.options"
@@ -33,7 +33,7 @@
             >
               <li
                 :class="[
-                  active ? 'bg-amber-100 text-amber-900' : 'text-gray-900',
+                  active ? 'bg-primary text-white' : 'text-gray-900',
                   'relative cursor-default select-none py-2 pl-10 pr-4',
                 ]"
               >
@@ -48,7 +48,7 @@
                   v-if="selected"
                   class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
                 >
-                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                  <CheckIcon class="h-5 w-5 text-white" aria-hidden="true" />
                 </span>
               </li>
             </ListboxOption>
