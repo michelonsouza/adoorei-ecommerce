b958dba0f57d6afc0cbb6b20d9dad36a838b8e44
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 10:29:10 -0300

    feat: create HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,28 @@
 <template>
-  <h1>HomePage</h1>
+  <hero-banner />
+  <product-list :products="store.filteredProducts" />
 </template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+import { HeroBanner, ProductList } from '@/components';
+import { getProducts } from '@/services';
+import { useProductStore } from '@/store';
+
+const store = useProductStore();
+
+async function getAllProducts() {
+  if (!store.quantity) {
+    const result = await getProducts();
+    store.setProducts(result);
+  }
+}
+
+onMounted(() => {
+  getAllProducts();
+});
+</script>
 
 <script lang="ts">
 export default { name: 'HomePage' };
