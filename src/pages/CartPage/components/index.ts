6916889734781511b91baf572e2a7a847da249cd
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 20:27:03 -0300

    feat: export all components from cartpage/components folder

---
+++ src/pages/CartPage/components/index.ts
@@ -0,0 +1,4 @@
+export { default as CartProductItem } from './CartProductItem.vue';
+export { default as NoProducts } from './NoProducts.vue';
+export { default as SuccessModal } from './SuccessModal.vue';
+export { default as UserDetailsModal } from './UserDetailsModal.vue';
