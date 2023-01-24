f343fc5117d11e52977902c15a9f40250b56f1a1
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 12:20:27 -0300

    feat: add orderBy functionality in CategoryProductPage

--- src/pages/CategoryProductPage.vue
+++ src/pages/CategoryProductPage.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1 class="text-2xl mb-3">Category: {{ mappedCategory }}</h1>
+  <div
+    class="flex items-center justify-between mb-3 flex-col gap-2 sm:flex-row sm:gap-0"
+  >
+    <h1 class="text-2xl mb-3">Category: {{ mappedCategory }}</h1>
+    <select-input
+      :options="filterOptions"
+      :value="filterBy"
+      @change="handleFilter"
+    />
+  </div>
   <product-list v-if="!!filteredProducts.length" :products="filteredProducts" />
   <div v-else>
     <h1 class="text-3xl">
@@ -9,26 +18,52 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 import { useRoute } from 'vue-router';
 
-import { ProductList } from '@/components';
+import { ProductList, SelectInput, SelectOptionType } from '@/components';
 import { CategoryTagEnum, CategoryTagKeys } from '@/enums';
+import type { Product } from '@/models';
 import { useProductStore } from '@/store';
+import { sortProductsByKey } from '@/utils';
 
 const route = useRoute();
 const store = useProductStore();
+
+const filterOptions: SelectOptionType<string>[] = [
+  {
+    label: 'Order by',
+    value: '',
+  },
+  {
+    label: 'Name',
+    value: 'title',
+  },
+  {
+    label: 'Price',
+    value: 'price',
+  },
+  {
+    label: 'Rating',
+    value: 'rating',
+  },
+];
+const filterBy = ref<SelectOptionType<string>>(filterOptions[0]);
 
 const mappedCategory = computed(
   () => CategoryTagEnum[route.params.category as CategoryTagKeys],
 );
 
 const filteredProducts = computed(() => {
-  return store.products.filter(
-    product => product.category === String(mappedCategory.value),
-  );
+  return [...store.products]
+    .filter(product => product.category === String(mappedCategory.value))
+    .sort(sortProductsByKey(filterBy.value.value as Partial<keyof Product>));
 });
+
+function handleFilter(value: SelectOptionType) {
+  filterBy.value = value as SelectOptionType<string>;
+}
 </script>
 
 <script lang="ts">
