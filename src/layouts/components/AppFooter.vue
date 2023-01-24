04f080fa86650e0843175e440341b5d9168d08e0
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 10:17:40 -0300

    feat: create AppFooter component

---
+++ src/layouts/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="flex justify-center dark:shadow-gray-700 mt-5">
+    <div class="container h-full flex py-6 items-center justify-center">
+      <p class="font-bold dark:text-primary">ADOOREI &copy;{{ year }}</p>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+const year = new Date().getFullYear();
+</script>
+
+<script lang="ts">
+export default {
+  name: 'AppFooter',
+};
+</script>
