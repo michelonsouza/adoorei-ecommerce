1fa02a486e2403f674cd250a5eef5abe46c63ce4
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 12:19:30 -0300

    feat: create CartPage

---
+++ src/pages/CartPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>CartPage</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import { useCartStore } from '@/store';
+
+// const cartStore = useCartStore();
+</script>
+
+<script lang="ts">
+export default {
+  name: 'CartPage',
+};
+</script>
