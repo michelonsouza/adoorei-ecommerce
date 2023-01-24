0f384ebfde164a0f231f68a07efcff2a04bf4a31
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 12:18:38 -0300

    feat: add cart page into router

--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,13 @@ import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
 
 import { HomePage } from './pages';
 
-const ProductPage = () => import('./pages/ProductPage.vue');
+const CartPage = () => import('./pages/CartPage.vue');
 const CategoryProductPage = () => import('./pages/CategoryProductPage.vue');
+const ProductPage = () => import('./pages/ProductPage.vue');
 
 const routes: Readonly<RouteRecordRaw[]> = [
   { path: '/', component: HomePage },
+  { path: '/cart', component: CartPage },
   { path: '/:category/products', component: CategoryProductPage },
   { path: '/product/:id', component: ProductPage },
 ];
