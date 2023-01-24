057aafe5bec7e037ecd57518c96b2ce172cf913f
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 12:20:53 -0300

    feat: export CartPage from pages folder

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
+export { default as CartPage } from './CartPage.vue';
 export { default as CategoryProductPage } from './CategoryProductPage.vue';
 export { default as HomePage } from './HomePage.vue';
 export { default as ProductPage } from './ProductPage.vue';
