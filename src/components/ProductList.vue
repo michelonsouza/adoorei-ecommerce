a7340a4c938b0352d7cc7af12ea252c9722739ba
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 10:14:04 -0300

    feat: create ProductCardList component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mt-4"
+  >
+    <product-card
+      v-for="product in props.products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ProductCard } from '@/components';
+import type { Product } from '@/models';
+
+interface ProductListProps {
+  products: Product[];
+}
+
+const props = defineProps<ProductListProps>();
+</script>
+
+<script lang="ts">
+export default {
+  name: 'ProductList',
+};
+</script>
