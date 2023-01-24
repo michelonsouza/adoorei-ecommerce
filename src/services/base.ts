564602e32381a9b52eb6dce82330c236fa8beab2
Michelon Souza <michelon.souza@hotmail.com>
2023-01-24 10:21:26 -0300

    fix: adjust base service

--- src/services/base.ts
+++ src/services/base.ts
@@ -1,24 +1,10 @@
-import axios, {
-  AxiosRequestConfig,
-  RawAxiosRequestHeaders,
-  AxiosHeaders,
-} from 'axios';
+import axios from 'axios';
 
-import { UnauthorizedError } from '@/errors';
 import { encryptStorage } from '@/utils';
-
-type InterceptorAxiosHeaders = (RawAxiosRequestHeaders | AxiosHeaders) & {
-  Authorization?: string;
-  authorization?: string;
-};
-
-interface InterceptorAxiosRequestConfig extends AxiosRequestConfig {
-  headers?: InterceptorAxiosHeaders;
-}
 
 const token = encryptStorage.getItem<string>('token');
 
-const baseService = axios.create({
+export const baseService = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
   headers: {
     common: {
@@ -26,25 +12,3 @@ const baseService = axios.create({
     },
   },
 });
-
-baseService.interceptors.request.use(
-  (config: InterceptorAxiosRequestConfig) => {
-    const authorization =
-      config?.headers?.Authorization || config?.headers?.authorization;
-    const newToken = encryptStorage.getItem<string>('auth_token');
-    const internConfig = { ...config };
-
-    if (!authorization && !newToken) {
-      throw new UnauthorizedError();
-    }
-
-    if (internConfig?.headers) {
-      internConfig.headers.Authorization = newToken;
-    }
-
-    return internConfig;
-  },
-  error => Promise.reject(error),
-);
-
-export { baseService };
